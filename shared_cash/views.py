6976d6c500dffae0c1a302edcc8d79afa6138ac4
Alexander Sommer <git@alexandersommer.eu>
2020-06-05 14:16:29 +0200

    Remove double slashes in file link
    This worked for some reason but looked kind of wrong tbh.

--- shared_cash/views.py
+++ shared_cash/views.py
@@ -6,13 +6,13 @@ from django.shortcuts import redirect
 if settings.DEBUG:
     @login_required
     def file_download(request, filename):
-        return redirect(f'{settings.PROTECTED_MEDIA}/{filename}')
+        return redirect(f'{settings.PROTECTED_MEDIA}{filename}')
 else:
     @login_required
     def file_download(request, filename):
         response = HttpResponse()
         response['Content-Type'] = 'application/pdf'
-        response['X-Accel-Redirect'] = f'{settings.PROTECTED_MEDIA}/{filename}'
+        response['X-Accel-Redirect'] = f'{settings.PROTECTED_MEDIA}{filename}'
         response['Content-Disposition'] = f'inline;filename={filename}'
 
         return response
